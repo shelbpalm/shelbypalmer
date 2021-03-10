6cb947c9659a356511320ece6da184761e628d4a
shelbpalm <75814643+shelbpalm@users.noreply.github.com>
2021-03-09 20:12:09 -0800

    Added Portfolio pieces and attempted lightGallery lightGallery link added to html head, and js doc.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,22 @@
 console.log("OK");
 
-// Store Background Classes
-const backgrounds = ["bg0", "bg1", "bg2"];
+// // Store Background Classes
+// const backgrounds = ["bg0", "bg1", "bg2"];
 
-// Get Random Number
-var numRand;
-var max = 3;
-numRand = Math.floor(Math.random() * backgrounds.length);
+// // Get Random Number
+// var numRand;
+// var max = 3;
+// numRand = Math.floor(Math.random() * backgrounds.length);
 
-// Get element
-let pageBackground = document.querySelector("body");
+// // Get element
+// let pageBackground = document.querySelector("body");
 
-// Set class name from stored values
-pageBackground.className = backgrounds[numRand];
+// // Set class name from stored values
+// pageBackground.className = backgrounds[numRand];
+
+//lightGallery
+const gallery = document.getElementById("gallery");
+
+lightGallery(gallery, {
+  //options
+});
